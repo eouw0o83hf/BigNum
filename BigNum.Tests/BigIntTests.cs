9d2deced0d9e74d4d75b5b5d47a25ed3c570e91d
Nathan Landis <nlandis@spotlite.com>
2014-11-23 13:06:31 -0600

    Support negative numbers in and out

--- BigNum.Tests/BigIntTests.cs
+++ BigNum.Tests/BigIntTests.cs
@@ -23,5 +23,12 @@ namespace BigNum.Tests
             var sut = new BigInt(8429750293);
             Assert.That(sut.ToString(), Is.EqualTo("8429750293"));
         }
+
+        [Test]
+        public void InitializeAndString_Negative()
+        {
+            var sut = new BigInt(-38202430);
+            Assert.That(sut.ToString(), Is.EqualTo("-38202430"));
+        }
     }
 }
--- BigNum/BigInt.cs
+++ BigNum/BigInt.cs
@@ -10,13 +10,23 @@ namespace BigNum
     {
         // Little-Endian BCDs
         private readonly byte[] _bytes;
+        private readonly bool _negative;
         
         public BigInt(long input)
         {
+            // Main edge case
             if (input == 0)
             {
                 _bytes = new byte[] { 0 };
                 return;
+            }
+
+            // If it's negative, we'll track it with a bool, then
+            // funnel it through the same logic as a positive
+            if (input < 0)
+            {
+                _negative = true;
+                input = -input;
             }
 
             // We're storing BCDs in each array member
@@ -29,11 +39,18 @@ namespace BigNum
 
         public override string ToString()
         {
-            var charArray = _bytes
-                                .Reverse()
-                                .Select(a => (char) (a + '0'))
-                                .ToArray();
-            return new string(charArray);
+            // Put a negative sign in the first position if we need it
+            var negativeSign = _negative ? new [] { '-' } : Enumerable.Empty<char>();
+
+            // Bytes are little-endian, so we have to go in reverse order.
+            // Adding '0' to the digit is an ASCII hack that gets the int value for ASCII '0',
+            // then adds an offset for the current digit.
+            var digits = _bytes
+                            .Reverse()
+                            .Select(a => (char) (a + '0'));
+
+            // Concat() instead of Union() because we need repeated digits to be preserved
+            return new string(negativeSign.Concat(digits).ToArray());
         }
     }
 }
